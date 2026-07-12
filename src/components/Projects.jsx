9712e2eb908415aabebc679e56f85f72e4a247b7
AndiZogaj22 <andiolsizogaj@gmail.com>
2026-07-12 02:41:15 +0200

    hive blog added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,6 +16,7 @@ import p13 from '../assets/p13.PNG';
 import p14 from '../assets/p14.PNG'
 import p15 from '../assets/p15.PNG'
 import p16 from '../assets/p16.PNG'
+import p17 from '../assets/p17.PNG'
 
 import { FaHtml5, FaCss3Alt, FaJs, FaReact } from 'react-icons/fa';
 import { BiLogoMongodb, BiLogoTailwindCss, BiLogoUnity, BiLogoPhp } from "react-icons/bi";
@@ -142,6 +143,14 @@ const Projects = () => {
       description: "A freelancing project in which the client required a website for their metal fabrication and and industrial solutions company.",
       image: p15,
       visitLink: "https://www.comogrit.com/",
+
+    },
+    {
+      name: "Hive Blog",
+      techStack: [<FaReact size={36} className="text-blue-400 dark:text-white" title="next" />, <BiLogoTailwindCss size={36} className="text-blue-400" title="tailwind" />],
+      description: "A freelancing project in which the client required a website for their online blog.",
+      image: p17,
+      visitLink: "https://hiveblog-id1pn5vws-andizogaj22s-projects.vercel.app/",
 
     },
 
